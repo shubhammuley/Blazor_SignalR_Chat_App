4175bf6e39b262d4c43b533657b951a65cc5bb79
shubhammuley <muley3282@gmail.com>
2024-03-06 00:54:02 +0530

    Add implementation for ChatService

--- BlazorChatWebApp/BlazorChatWebApp.Client/ChatServices/ChatService.cs
+++ BlazorChatWebApp/BlazorChatWebApp.Client/ChatServices/ChatService.cs
@@ -1,7 +1,41 @@
-﻿namespace BlazorChatWebApp.Client.ChatServices
+﻿using ChatModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorChatWebApp.Client.ChatServices
 {
     public class ChatService
     {
+        public Action? InvokeChatDisplay { get; set; }
 
+        public List<Chat> Chats { get; set; } = [];
+        private readonly HubConnection _hubConnection;
+        public bool IsConnected { get; set; }
+
+        public ChatService(NavigationManager navigationManager)
+        {
+            _hubConnection = new HubConnectionBuilder()
+        .WithUrl(navigationManager.ToAbsoluteUri("/chathub")).Build();
+        }
+
+        public void ReceiveMessage()
+        {
+            _hubConnection.On<Chat>("ReceiveMessage", (chat) =>
+            {
+                Chats.Add(chat);
+                InvokeChatDisplay?.Invoke();
+            });
+        }
+
+        public async Task StartConnectionAsync()
+        {
+            await _hubConnection.StartAsync();
+            IsConnected = _hubConnection!.State == HubConnectionState.Connected;
+        }
+
+        public Task SendChat(Chat chat)
+        {
+            _hubConnection!.SendAsync("SendMessage", chat);
+        }
     }
 }
