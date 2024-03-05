76ff62affb037bd95a8afc1d58d1af9b1381f92e
shubhammuley <muleyshubham20@gmail.com>
2024-03-05 22:20:55 +0530

    Add SendMessage method in ChatHub class to send message to all clients

--- BlazorChatWebApp/BlazorChatWebApp/ChatHubs/ChatHub.cs
+++ BlazorChatWebApp/BlazorChatWebApp/ChatHubs/ChatHub.cs
@@ -5,6 +5,15 @@ namespace BlazorChatWebApp.ChatHubs
     public class ChatHub : Hub
     {
 
+        public async Task SendMessage(string userName, string message, DateTime date)
+        {
+            await Clients.All.SendAsync("Receive Message",userName, message, date);
+        }
+
+
+
+
+
 
     }
 }
