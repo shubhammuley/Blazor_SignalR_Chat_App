c8142d47dc29c73e5e73053ed2131d3e913266d0
shubhammuley <muley3282@gmail.com>
2024-03-07 16:50:42 +0530

    Consume chatRepo in  chathub

--- BlazorChatWebApp/BlazorChatWebApp/ChatHubs/ChatHub.cs
+++ BlazorChatWebApp/BlazorChatWebApp/ChatHubs/ChatHub.cs
@@ -1,13 +1,15 @@
-﻿using ChatModels;
+﻿using BlazorChatWebApp.Migrations.Repos;
+using ChatModels;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorChatWebApp.ChatHubs
 {
-    public class ChatHub : Hub
+    public class ChatHub(ChatRepo chatRepo) : Hub
     {
 
         public async Task SendMessage(Chat chat)
         {
+            await chatRepo.SaveChatAsync(chat);
             await Clients.All.SendAsync("ReceiveMessage",chat);
         }
 
