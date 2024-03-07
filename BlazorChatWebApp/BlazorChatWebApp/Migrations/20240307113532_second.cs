8ba90dd2f43165cf580b075da6194f85271a8ad6
shubhammuley <muley3282@gmail.com>
2024-03-07 17:12:58 +0530

    Fix for Migration error. Error : Primary key missing. Added Id key in Chat class.

---
+++ BlazorChatWebApp/BlazorChatWebApp/Migrations/20240307113532_second.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorChatWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Chats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Chats",
+                table: "Chats",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Chats",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Chats");
+        }
+    }
+}
--- ChatModels/Chat.cs
+++ ChatModels/Chat.cs
@@ -4,9 +4,11 @@ using System.ComponentModel.DataAnnotations;
 namespace ChatModels
 {
 
-    [Keyless]
+
     public class Chat
     {
+
+        public int Id { get; set; }
         [Required]
         public string? Message { get; set; }
 
