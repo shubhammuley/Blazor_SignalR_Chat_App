e06ae2a713ff6ce05b6b3d6be9535ef07450740a
shubhammuley <muley3282@gmail.com>
2024-03-07 16:49:00 +0530

    Add ChatController

---
+++ BlazorChatWebApp/BlazorChatWebApp/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using BlazorChatWebApp.Migrations.Repos;
+using ChatModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorChatWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController(ChatRepo chatRepo) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Chat>>> GetChatAsync() =>
+            Ok(await chatRepo.GetChatAsync());
+
+    }
+}
