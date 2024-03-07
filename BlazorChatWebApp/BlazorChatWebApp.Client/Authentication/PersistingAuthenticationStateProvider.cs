befe67634dab7878a398c1d60806977e56d1b7d3
shubhammuley <muley3282@gmail.com>
2024-03-08 00:36:56 +0530

    Add PersistingAuthenticationState in client. Register PersistingAuthenticationState.

---
+++ BlazorChatWebApp/BlazorChatWebApp.Client/Authentication/PersistingAuthenticationStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using BlazorChatWebApp.Client.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+
+
+namespace BlazorChatWebApp.Client.Authentication
+{
+    public class PersistingAuthenticationStateProvider : AuthenticationStateProvider
+    {
+
+        private static readonly Task<AuthenticationState> defaultAuthenticationTask =
+            Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(
+                ))));
+
+        private readonly Task<AuthenticationState?> authenticationStateTask
+            = defaultAuthenticationTask;
+
+
+        public PersistingAuthenticationStateProvider(PersistentComponentState state)
+        {
+            if (!state.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+            {
+
+                return;
+            }
+
+            Claim[] claims = [
+                new Claim(ClaimTypes.NameIdentifier, userInfo.Id!),
+                new Claim(ClaimTypes.Email, userInfo.Email!),
+                new Claim(ClaimTypes.Name, userInfo.FullName!)
+                ];
+
+            authenticationStateTask = Task.FromResult(
+                new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, nameof(PersistingAuthenticationStateProvider))))
+                )!;
+        }
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        => authenticationStateTask!;
+    }
+}
--- BlazorChatWebApp/BlazorChatWebApp.Client/Program.cs
+++ BlazorChatWebApp/BlazorChatWebApp.Client/Program.cs
@@ -1,9 +1,14 @@
+using BlazorChatWebApp.Client.Authentication;
 using BlazorChatWebApp.Client.ChatServices;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<ChatService>();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingAuthenticationStateProvider>();
+
 builder.Services.AddScoped(
     sp => new HttpClient
     {
