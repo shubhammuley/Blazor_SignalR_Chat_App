10a309e9302c7dc09cca55b09e3603a10ba8672b
shubhammuley <muley3282@gmail.com>
2024-03-07 16:52:39 +0530

    Register http service for client

--- BlazorChatWebApp/BlazorChatWebApp.Client/Program.cs
+++ BlazorChatWebApp/BlazorChatWebApp.Client/Program.cs
@@ -4,5 +4,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<ChatService>();
+builder.Services.AddScoped(
+    sp => new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    }
+    ); 
+
 
 await builder.Build().RunAsync();
