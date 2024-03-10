76efb34c6ac80e36047f7a9211014be35bc827a0
shubhammuley <muley3282@gmail.com>
2024-03-10 11:30:35 +0530

    Fix for group-chat

--- BlazorChatWebApp/BlazorChatWebApp/Migrations/20240308170541_third.Designer.cs
+++ BlazorChatWebApp/BlazorChatWebApp/Migrations/20240308170541_third.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorChatWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorChatWebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240308170541_third")]
+    partial class third
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
